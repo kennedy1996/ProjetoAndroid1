6949967efe3bd41778cb5b2d0a457f8353f68c47
kennedy1996 <87372917+kennedy1996@users.noreply.github.com>
2021-07-22 17:19:29 -0300

    Inserindo Confirmação para deletar
    Implementado ao clicar em Deletar se realmente deseja deletar.

--- Agenda2021/app/src/main/java/com/example/agenda2021/ui/activity/ListaAlunosActivity.java
+++ Agenda2021/app/src/main/java/com/example/agenda2021/ui/activity/ListaAlunosActivity.java
@@ -1,5 +1,8 @@
 package com.example.agenda2021.ui.activity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -33,6 +36,8 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setTitle(TITULO_APPBAR);
         configuraFabNovoAluno();
         configuraLista();
+
+
                /*
 
         List<String> alunos = new ArrayList<>(
@@ -71,14 +76,28 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         int itemId = item.getItemId();
         if(itemId == R.id.activity_lista_alunos_menu_remover){
+            confirmaRemocao(item);
 
-            AdapterView.AdapterContextMenuInfo  menuInfo =
-                    (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-            Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
-            remove(alunoEscolhido);
         }
 
         return super.onContextItemSelected(item);
+    }
+
+    private void confirmaRemocao(final MenuItem item) {
+        new AlertDialog.Builder(this)
+                .setTitle("Removendo Aluno")
+                .setMessage("Tem certeza que deseja remover o aluno")
+                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AdapterView.AdapterContextMenuInfo  menuInfo =
+                                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                        Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
+                        remove(alunoEscolhido);
+                    }
+                })
+                .setNegativeButton("Não", null)
+                .show();
     }
 
     private void configuraLista() {
